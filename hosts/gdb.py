ad8b57e529b2a9163a63edc439825490f6fb6862
Philip Karlsson <philip.karlsson@husqvarnagroup.com>
2019-10-26 23:02:25 +0200

    First working version

--- hosts/gdb.py
+++ hosts/gdb.py
@@ -2,3 +2,24 @@ import gdb
 import re
 import zmq
 import msgpack
+
+def stop_event (event):
+    sal = gdb.selected_frame().find_sal()
+
+    bp = {}
+    bp['type'] = 'bp_hit'
+    bp['file'] = sal.symtab.fullname()
+    bp['line'] = sal.line
+
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5678")
+
+    bp_data = msgpack.packb(bp, use_bin_type=True)
+    socket.send(bp_data)
+    raw_resp = socket.recv()
+
+    msg = msgpack.unpackb(raw_resp, raw=False)
+    print(msg)
+
+gdb.events.stop.connect(stop_event)
---
+++ test/run.gdb
@@ -0,0 +1,3 @@
+so ../hosts/gdb.py
+b main.c:6
+r
